7e8554bdb685d768fdf3b5c570f4479d5f2735f1
Venktesh Sharma <venkteshsharma25@gmail.com>
2026-07-22 23:42:43 +0530

    Add files via upload

---
+++ Hashing/HS3.java
@@ -0,0 +1,17 @@
+package Hashing;
+
+import java.util.TreeSet;
+
+/**
+ * HS3
+ */
+public class HS3 {
+    public static void main(String[] args) {
+        TreeSet <String> set = new TreeSet<>() ; 
+        set.add("Java");
+        set.add("Python");
+        set.add("C++");
+        System.out.println(set);
+    }
+    
+}
