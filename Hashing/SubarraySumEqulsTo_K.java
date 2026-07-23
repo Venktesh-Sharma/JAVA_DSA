8cad83d92e3dde52728fccdbbc15f2f4f73349b1
Venktesh Sharma <venkteshsharma25@gmail.com>
2026-07-23 20:38:21 +0530

    Add files via upload

---
+++ Hashing/SubarraySumEqulsTo_K.java
@@ -0,0 +1,37 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class SubarraySumEqulsTo_K {
+    public static int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            sum += nums[i];
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+     public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3};
+        int k = 3;
+
+        int ans = subarraySum(nums, k);
+
+        System.out.println("Number of subarrays = " + ans);
+    }
+}
